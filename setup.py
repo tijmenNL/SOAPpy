64125a24aad228761f38312d44bde4bec7354276
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-05-15 12:39:48 +0200

    fix cve CVE Requests

--- setup.py
+++ setup.py
@@ -11,53 +11,13 @@ def read(*rnames):
     return "\n"+ open(
         os.path.join('.', *rnames)
     ).read()
-
-
-
-
-def load_version():
-    """
-    Load the version number by executing the version file in a variable. This
-    way avoids executing the __init__.py file which load nearly everything in
-    the project, including fpconst which is not yet installed when this script
-    is executed.
-
-    Source: https://github.com/mitsuhiko/flask/blob/master/flask/config.py#L108
-    """
-
-    import imp
-    from os import path
-
-    filename = path.join(path.dirname(__file__), 'src', 'SOAPpy', 'version.py')
-    d = imp.new_module('version')
-    d.__file__ = filename
-
-    try:
-        execfile(filename, d.__dict__)
-    except IOError, e:
-        e.strerror = 'Unable to load the version number (%s)' % e.strerror
-        raise
-
-    return d.__version__
-
-
-__version__ = load_version()
-
-
 url="https://github.com/kiorky/SOAPpy.git"
-
 long_description="SOAPpy provides tools for building SOAP clients and servers.  For more information see " + url\
     +'\n'+read('README.txt')\
-    +'\n'+read('CHANGES.txt')\
-
-if CVS:
-    import time
-    __version__ += "_CVS_"  + time.strftime('%Y_%m_%d')
-
-
+    +'\n'+read('CHANGES.txt')
 setup(
     name="SOAPpy",
-    version=__version__,
+    version='0.12.19.dev0',
     description="SOAP Services for Python",
     maintainer="Gregory Warnes, kiorky",
     maintainer_email="Gregory.R.Warnes@Pfizer.com, kiorky@cryptelium.net",
@@ -68,6 +28,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'wstools',
+        'defusedxml',
     ]
 )
 
